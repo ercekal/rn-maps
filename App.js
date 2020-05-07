a8d90a4f55e5cd6f4ae457b96fa14c8ee8d18ffa
Erce Kal <erce@futr.ai>
2020-05-07 13:49:28 +0100

    sorting the navigation and headers

--- App.js
+++ App.js
@@ -1,18 +1,50 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 import AuthScreen from './screens/AuthScreen'
 import WelcomeScreen from './screens/WelcomeScreen'
+import MapScreen from './screens/MapScreen'
+import DeckScreen from './screens/DeckScreen'
+import ReviewScreen from './screens/ReviewScreen'
+import SettingsScreen from './screens/SettingsScreen'
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   const Tab = createBottomTabNavigator();
+  const Stack = createStackNavigator();
 
+  function ReviewStack() {
+    return (
+      <Stack.Navigator>
+        <Stack.Screen
+          name="review"
+          component={ReviewScreen}
+          options={{
+            title: 'Review Jobs',
+          }}/>
+        <Stack.Screen name="settings" component={SettingsScreen} />
+      </Stack.Navigator>
+    );
+  }
+
+  const Main = () => {
+    return (
+      <NavigationContainer independent={true}>
+        <Tab.Navigator>
+          <Tab.Screen name="map" component={MapScreen} />
+          <Tab.Screen name="deck" component={DeckScreen} />
+          <Tab.Screen name="review" component={ReviewStack} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    )
+  }
   return (
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Welcome" component={WelcomeScreen} />
         <Tab.Screen name="Auth" component={AuthScreen} />
+        <Tab.Screen name="Main" component={Main} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- screens/ReviewScreen.js
+++ screens/ReviewScreen.js
@@ -1,10 +1,31 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Platform } from 'react-native';
+import { Button,  } from 'react-native-elements'
 
-const ReviewScreen = () => (
-    <View>
-        <Text>ReviewScreen</Text>
-    </View>
-);
+const ReviewScreen = ({navigation, route}) => {
+
+  React.useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitleStyle: { flex: 1, textAlign: 'center', color: 'black', fontSize: 20, alignSelf: 'center' , },
+      headerTintColor: 'white',
+      headerStyle: {
+          backgroundColor: 'white',
+      },
+      headerTitleAlign: 'center',
+      headerRight: () => (
+        <Button
+          onPress={() => navigation.navigate('settings')}
+          type="clear"
+          title="Settings"
+        />
+      ),
+      // style: {
+      //   marginTop: Platform.OS === 'android' ? 24 : 0
+      // }
+    });
+  }, [navigation]);
+
+  return <Text>Count</Text>;
+}
 
 export default ReviewScreen;
